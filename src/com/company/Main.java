c35c0c4b5edd416bbe2217cf96ff6ff7a3b4b31d
thilcgit <tsnew19@gmail.com>
2023-04-20 03:27:00 +0530

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("results are ");
         System.out.println(hasSameLastDigit(41,22,70));
     }
 
     public static boolean hasSameLastDigit(int para1, int para2, int para3) {
+        System.out.println("----------------------------");
         if (para1 < 10 || para1 > 1000 || para2 < 10 || para2 > 1000 || para3 < 10 || para3 > 1000) {
             return false;
         }
